3ddd05f65473837532de44e31fc58fc0377b525b
XOMAK12 <163844436+XOMAK12@users.noreply.github.com>
2024-06-10 19:24:43 +0600

    Add files via upload

--- js/home_works.js
+++ js/home_works.js
@@ -19,14 +19,60 @@ gmailButton.onclick = () => {
 // MOVE BLOCK
 
 const childBlock = document.querySelector('.child_block');
+const parentBlock = document.querySelector('.parent_block');
 
-let positionLeft = '';
-function moveBlock () {
-    if (positionLeft < 448){
-        positionLeft++
+let positionLeft = 0;
+let positionTop = 0;
+let offsetParentWidth = parentBlock.offsetWidth - childBlock.offsetWidth;
+let offsetParentHeight = parentBlock.offsetHeight - childBlock.offsetHeight;
+
+function moveBlock() {
+    if (positionLeft < offsetParentWidth && positionTop === 0) {
+        positionLeft++;
         childBlock.style.left = `${positionLeft}px`;
-        requestAnimationFrame(moveBlock)
+        requestAnimationFrame(moveBlock);
+    } else if (positionLeft >= offsetParentWidth && positionTop < offsetParentHeight) {
+        positionTop++;
+        childBlock.style.top = `${positionTop}px`;
+        requestAnimationFrame(moveBlock);
+    } else if (positionTop >= offsetParentHeight && positionLeft > 0) {
+        positionLeft--;
+        childBlock.style.left = `${positionLeft}px`;
+        requestAnimationFrame(moveBlock);
+    } else if (positionLeft === 0 && positionTop > 0) {
+        positionTop--;
+        childBlock.style.top = `${positionTop}px`;
+        requestAnimationFrame(moveBlock);
     }
 }
-
 moveBlock();
+
+//STOPWATCH
+
+const secondBlock = document.querySelector('#seconds');
+const startBtn = document.querySelector('#start');
+const stopBtn = document.querySelector('#stop');
+const resetBtn = document.querySelector('#reset');
+let secondNum = 0;
+let interval;
+
+startBtn.onclick = () => {
+    if (!interval) {
+        interval = setInterval(() => {
+            secondNum++;
+            secondBlock.innerHTML = secondNum;
+        }, 1000);
+    }
+};
+
+stopBtn.onclick = () => {
+    clearInterval(interval)
+    interval = null;
+}
+
+resetBtn.onclick = () => {
+    secondBlock.innerHTML = '0';
+    secondNum = 0
+    clearInterval(interval)
+    interval = null;
+}
