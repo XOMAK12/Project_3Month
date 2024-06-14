9153b30f641905e051475108a5e32a5c6baeb28b
XOMAK12 <nnikitaivanov202@gmail.com>
2024-06-14 22:13:26 +0600

    1

--- js/lesson.js
+++ js/lesson.js
@@ -15,3 +15,36 @@ phoneButton.onclick = () => {
         phoneResult.style.color = 'red'
     }
 }
+
+// TAB SLIDER
+
+const tabContentBlocks = document.querySelectorAll('.tab_content_block')
+const tabContentItems = document.querySelectorAll('.tab_content_item')
+const tabParent = document.querySelector('.tab_content_items')
+const hideTabContent = () => {
+    tabContentBlocks.forEach((item) => {
+        item.style.display = 'none'
+    })
+    tabContentItems.forEach((item) => {
+        item.classList.remove('tab_content_item_active')
+    })
+}
+
+const showTabContent = (index = 0) => {
+    tabContentBlocks[index].style.display = 'block'
+    tabContentItems[index].classList.add('tab_content_item_active')
+}
+
+hideTabContent()
+showTabContent()
+
+tabParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabContentItems.forEach((item, index) => {
+            if (event.target === item) {
+                hideTabContent()
+                showTabContent(index)
+            }
+        })
+    }
+}
